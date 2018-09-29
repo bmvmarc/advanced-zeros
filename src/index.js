26c41fbc2d48399e7345dabb5e129af8b82c6783
Marina Chichel <bmvmarc@gmail.com>
2018-09-29 21:43:19 +0300

    advanced zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+    let res = 100000000;
+    for (let i = 2; i <= base; i++) {
+        if (base % i === 0) {
+            count1 = 0;
+            while (base % i === 0) {
+                count1++;
+                base /= i;
+            }
+            count2 = getMultipliersCount(number, i);
+            res = Math.min(res, count2 / count1)
+        }
+    }
+    return Math.floor(res);
+
 }
+
+function getMultipliersCount(number, div) {
+
+    let sum = 0, divider = div;
+    while (true) {
+        quantity = (number - number % divider) / divider;
+        if (quantity > 0) {
+            sum += quantity;
+            divider *= div;
+        }
+        else {
+            break;
+        }
+    }
+    return sum;
+}
+
+
+
